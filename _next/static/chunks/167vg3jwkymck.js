243454d9d4a1784a9ed2fd423de4523be0bf5318
daniil.chekov <Chekov.99@mail.ru>
2026-08-21 15:30:04 +0700

    Обновить preview NRAV из 480c84c94e55

--- _next/static/chunks/167vg3jwkymck.js
+++
@@ -1,1 +0,0 @@
-(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,95499,o=>{o.v(t=>Promise.all(["static/chunks/0.31e6qdq4~ou.js"].map(t=>o.l(t))).then(()=>t(98113)))}]);
