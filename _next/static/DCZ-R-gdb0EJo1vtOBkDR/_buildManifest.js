0c8baf8516d44932125042996328e8d336d28abf
Кирилл <kirill@MacBook-Air-Kirill.local>
2026-08-26 22:58:21 +0700

    Обновить preview NRAV из 1f42018f4ea1

---
+++ _next/static/DCZ-R-gdb0EJo1vtOBkDR/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/NRAV-PWA/_versions/1f42018f4ea15749f86bde9593e694b4279786dd/_next/:path+",
+        "destination": "/NRAV-PWA/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
