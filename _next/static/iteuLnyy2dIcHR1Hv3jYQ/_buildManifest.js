2f95265aa4e3e7f68622f453665642c3974ddf5e
Кирилл <kirill@MacBook-Air-Kirill.local>
2026-08-28 11:16:20 +0700

    Опубликовать исправленные мобильные отступы карты

---
+++ _next/static/iteuLnyy2dIcHR1Hv3jYQ/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/NRAV-PWA/_versions/d72d7b6933225ace4c82e047f371ba739178aac2/_next/:path+",
+        "destination": "/NRAV-PWA/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
