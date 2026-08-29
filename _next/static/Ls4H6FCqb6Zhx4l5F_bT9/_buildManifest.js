37d01c2556c0e02fe939f27fbe16602fe1d052c0
daniil.chekov <Chekov.99@mail.ru>
2026-08-29 09:51:05 +0700

    Обновить preview NRAV из 75a686db258b

---
+++ _next/static/Ls4H6FCqb6Zhx4l5F_bT9/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/NRAV-PWA/_versions/75a686db258bf2cb4dfc06ec63c121e4db591b2e/_next/:path+",
+        "destination": "/NRAV-PWA/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
