0529bfb6d7ebe229b6748ddb58d4719abb965cbb
Кирилл <kirill@MacBook-Air-Kirill.local>
2026-09-02 16:50:28 +0700

    Обновить preview NRAV из e14d1d866094

---
+++ _next/static/GUc4FZO_vcbJKhcHPCJ8L/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/NRAV-PWA/_versions/e14d1d866094fdcbc8534b7c8b057459084b7f67/_next/:path+",
+        "destination": "/NRAV-PWA/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
