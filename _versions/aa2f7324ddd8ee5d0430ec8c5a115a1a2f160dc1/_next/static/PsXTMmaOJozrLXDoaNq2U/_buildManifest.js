b662a789b8a7a891e7a7c903b5882d592d8f353b
Кирилл <kirill@MacBook-Air-Kirill.local>
2026-09-02 10:36:44 +0700

    Обновить preview NRAV из aa2f7324ddd8

---
+++ _versions/aa2f7324ddd8ee5d0430ec8c5a115a1a2f160dc1/_next/static/PsXTMmaOJozrLXDoaNq2U/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/NRAV-PWA/_versions/aa2f7324ddd8ee5d0430ec8c5a115a1a2f160dc1/_next/:path+",
+        "destination": "/NRAV-PWA/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
