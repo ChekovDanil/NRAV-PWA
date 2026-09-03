55e607277620e96fc8113de3b430d935616384f0
daniil.chekov <Chekov.99@mail.ru>
2026-09-03 16:54:24 +0700

    Обновить preview NRAV из b5130a81033e

---
+++ _next/static/QsG9S4T-03DHnrKB_gXT7/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/NRAV-PWA/_versions/b5130a81033ecf23160c85dd32840c2e2c335ad9/_next/:path+",
+        "destination": "/NRAV-PWA/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
