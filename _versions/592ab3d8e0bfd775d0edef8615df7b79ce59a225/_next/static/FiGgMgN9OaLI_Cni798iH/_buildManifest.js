72e688ab9e0bff03f8d7fdf2eec7e3ea12a7a02c
daniil.chekov <Chekov.99@mail.ru>
2026-09-03 18:08:58 +0700

    Обновить preview NRAV из 592ab3d8e0bf

---
+++ _versions/592ab3d8e0bfd775d0edef8615df7b79ce59a225/_next/static/FiGgMgN9OaLI_Cni798iH/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/NRAV-PWA/_versions/592ab3d8e0bfd775d0edef8615df7b79ce59a225/_next/:path+",
+        "destination": "/NRAV-PWA/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
