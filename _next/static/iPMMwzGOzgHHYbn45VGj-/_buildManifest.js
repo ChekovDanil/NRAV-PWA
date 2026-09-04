982342527a2eb90ede937aed41299c744afd1fc1
daniil.chekov <Chekov.99@mail.ru>
2026-09-04 18:59:14 +0700

    Обновить preview NRAV из 151fed21f772

---
+++ _next/static/iPMMwzGOzgHHYbn45VGj-/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/NRAV-PWA/_versions/151fed21f772f171c034c6d157a9286e7064a8f1/_next/:path+",
+        "destination": "/NRAV-PWA/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
