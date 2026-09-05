a2dddb2d0fb3f2d2e3df051d228e3d14f4abf29c
ChekovDanil <62105863+ChekovDanil@users.noreply.github.com>
2026-09-06 03:04:45 +0700

    Обновить PWA из NRAV 179d0e5c00f7

---
+++ _next/static/uMMK219EyL2pNYPMqbENc/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/NRAV-PWA/_versions/179d0e5c00f775e0b9724f3ad3181a100493d786/_next/:path+",
+        "destination": "/NRAV-PWA/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
