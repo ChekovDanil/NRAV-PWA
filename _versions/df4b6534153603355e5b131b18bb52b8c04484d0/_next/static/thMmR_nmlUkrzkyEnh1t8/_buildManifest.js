d7828900e376f0a29a5b32557218de19df9cd6b7
daniil.chekov <Chekov.99@mail.ru>
2026-09-05 13:06:31 +0700

    Обновить preview NRAV из df4b65341536

---
+++ _versions/df4b6534153603355e5b131b18bb52b8c04484d0/_next/static/thMmR_nmlUkrzkyEnh1t8/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/NRAV-PWA/_versions/df4b6534153603355e5b131b18bb52b8c04484d0/_next/:path+",
+        "destination": "/NRAV-PWA/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
