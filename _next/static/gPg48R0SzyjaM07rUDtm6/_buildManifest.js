c91443151b7947c9011993cdba0906209bca635e
ChekovDanil <62105863+ChekovDanil@users.noreply.github.com>
2026-09-05 22:34:23 +0700

    Обновить preview NRAV из 5d7608b77ff8

---
+++ _next/static/gPg48R0SzyjaM07rUDtm6/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/NRAV-PWA/_versions/5d7608b77ff80b28ed7e0f39244fe15ae11a2bae/_next/:path+",
+        "destination": "/NRAV-PWA/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
