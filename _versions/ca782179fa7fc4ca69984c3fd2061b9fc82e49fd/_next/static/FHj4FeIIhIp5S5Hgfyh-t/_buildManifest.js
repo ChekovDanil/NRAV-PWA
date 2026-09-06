c7fc94170a21aa09170f40f50d6f08e6f99d7dc2
Кирилл <kirill@MacBook-Air-Kirill.local>
2026-09-07 00:38:47 +0700

    Обновить preview NRAV из ca782179fa7f

---
+++ _versions/ca782179fa7fc4ca69984c3fd2061b9fc82e49fd/_next/static/FHj4FeIIhIp5S5Hgfyh-t/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/NRAV-PWA/_versions/ca782179fa7fc4ca69984c3fd2061b9fc82e49fd/_next/:path+",
+        "destination": "/NRAV-PWA/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
