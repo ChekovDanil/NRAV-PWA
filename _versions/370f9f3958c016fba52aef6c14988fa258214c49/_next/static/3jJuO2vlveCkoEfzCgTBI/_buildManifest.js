f32e528b8ace8bfb821a3f39c100c3e5cdf6759c
daniil.chekov <Chekov.99@mail.ru>
2026-09-10 11:37:27 +0700

    Обновить preview NRAV из 370f9f3958c0

---
+++ _versions/370f9f3958c016fba52aef6c14988fa258214c49/_next/static/3jJuO2vlveCkoEfzCgTBI/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/NRAV-PWA/_versions/370f9f3958c016fba52aef6c14988fa258214c49/_next/:path+",
+        "destination": "/NRAV-PWA/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
